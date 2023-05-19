70945a05a6f105e7c724dd87357d7694acc75302
simple <383559698@qq.com>
2023-05-19 17:02:24 +0800

    hook hookGetWifiInfo

--- checker-pine/src/main/java/me/simple/checker/hookers/WifiInfoHooker.java
+++ checker-pine/src/main/java/me/simple/checker/hookers/WifiInfoHooker.java
@@ -1,6 +1,7 @@
 package me.simple.checker.hookers;
 
 import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 
 import me.simple.checker.CheckerHelper;
 import top.canyie.pine.Pine;
@@ -9,17 +10,17 @@ import top.canyie.pine.callback.MethodHook;
 public class WifiInfoHooker {
 
     public static void hook() throws NoSuchMethodException {
-//        hookGetWifiInfo();
+        hookGetWifiInfo();
         hookGetMacAddress();
         hookGetSSID();
     }
 
     private static void hookGetWifiInfo() throws NoSuchMethodException {
-        Pine.hook(WifiInfo.class.getDeclaredMethod("getMacAddress"), new MethodHook() {
+        Pine.hook(WifiManager.class.getDeclaredMethod("getConnectionInfo"), new MethodHook() {
             @Override
             public void beforeCall(Pine.CallFrame callFrame) throws Throwable {
                 super.beforeCall(callFrame);
-                CheckerHelper.showWarn("getMacAddress");
+                CheckerHelper.showWarn("getConnectionInfo");
             }
         });
     }
